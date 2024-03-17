e4911ac964f7e08636ad27b0564704fcfd4591c0
Alexey Kulichevskiy <alexey.kulichevskiy+alexchevsky@gmail.com>
2024-03-17 09:45:22 +0100

    yet another dummy dag

---
+++ dags/dummy_dag_3.py
@@ -0,0 +1,38 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+
+# Define default arguments for the DAG
+default_args = {
+    'owner': 'Alex',
+    'depends_on_past': False,
+    'email': ['alex@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+# Define the DAG, its scheduling, and set it to start in the past
+dag = DAG(
+    'dummy_dag_3',
+    default_args=default_args,
+    description="You can't have too many dags!",
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2024, 3, 14),
+    catchup=False,
+)
+
+# Define the tasks
+start_task = DummyOperator(
+    task_id='start_task',
+    dag=dag,
+)
+
+end_task = DummyOperator(
+    task_id='end_task',
+    dag=dag,
+)
+
+# Set task dependencies
+start_task >> end_task
